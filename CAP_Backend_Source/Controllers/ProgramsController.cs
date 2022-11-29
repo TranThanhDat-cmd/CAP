02641641143b50de272f019af48743ea7bc7cd4a
TRAN THANH DAT <dat.tran@alta.com.vn>
2022-11-29 11:03:21 +0700

    Update Full Module Program

--- CAP_Backend_Source/Controllers/ProgramsController.cs
+++ CAP_Backend_Source/Controllers/ProgramsController.cs
@@ -1,6 +1,9 @@
-﻿using CAP_Backend_Source.Modules.Programs.Request;
+﻿using Azure.Core;
+using CAP_Backend_Source.Modules.Account.Request;
+using CAP_Backend_Source.Modules.Programs.Request;
 using CAP_Backend_Source.Modules.Programs.Service;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace CAP_Backend_Source.Controllers;
 
@@ -17,8 +20,30 @@ public class ProgramsController : ControllerBase
 
     [HttpPost]
     public async Task<IActionResult> Create([FromForm]CreateProgramRequest request)
-        => Ok(await _programService.CreateAsync(request));
-    
+    {
+        int id = int.Parse(User.FindFirstValue("id").ToString());
+        return Ok(await _programService.CreateAsync(id,request));
+    }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update([FromRoute] int id, [FromForm] CreateProgramRequest request)
+    {
+        return Ok(await _programService.UpdateAsync(id, request));
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete([FromRoute] int id)
+    {
+        await _programService.DeleteAsync(id);
+        return Ok();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get([FromRoute] int id)
+    {
+        return Ok(await _programService.GetDetailAsync(id));
+    }
+
+
     [HttpGet]
     public async Task<IActionResult> Get()
         => Ok(await _programService.GetAsync());
